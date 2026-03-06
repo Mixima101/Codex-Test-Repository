1ef94170087dfedefdb86e45125ac25a65860c89
Mixima101 <143468525+Mixima101@users.noreply.github.com>
2026-03-06 12:38:10 -0700

    Delete Dashboard 10 2 26 2026 directory

--- Dashboard 10 2 26 2026/Install_QuantumLeap.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set SCRIPT_DIR=%~dp0scripts\
-powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%install_quantumleap.ps1"
-if %errorlevel% neq 0 (
-  echo.
-  echo Install failed. Press any key to close...
-  pause >nul
-)
